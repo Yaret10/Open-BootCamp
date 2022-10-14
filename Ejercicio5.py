d3efbdcb160a8e50ce3a0f692bbec4cf09ef1786
Yaret Chuica <yaretchuica10@gmail.com>
2022-10-14 11:38:46 -0500

    Ejercicio5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,7 +1,3 @@
-#Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
-for i in range(100,0, -1):
-    print(i)
-
 #Escribe una función que pueda decirte si un año (número entero) es bisiesto o no.
 def año_bisiesto(año):
     if año % 4 == 0 and (año % 100 != 0 or año % 400 == 0):
